e2f13633dd4bf4e0a9ae7d63d44695d623e65984
Somkiat <somkiat.p@gmail.com>
2013-11-15 11:58:09 +0700

    Disable test

--- src/test/java/demo/testing/WithoutLoopTest.java
+++ src/test/java/demo/testing/WithoutLoopTest.java
@@ -8,13 +8,13 @@ import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(DataDrivenTestRunner.class)
+//@RunWith(DataDrivenTestRunner.class)
 public class WithoutLoopTest {
 
-	@Test
-	@DataLoader(filePaths = { "data.csv" })
-	@Repeat(times=500)
-	@Duration(timeInMillis=1)
+//	@Test
+//	@DataLoader(filePaths = { "data.csv" })
+//	@Repeat(times=500)
+//	@Duration(timeInMillis=1)
 	public void testUseRepeat10Times(@Param(name="ID") int id,  @Param(name="Name") String name) throws Exception {
 		System.out.println("Test=" + id);
 	}
