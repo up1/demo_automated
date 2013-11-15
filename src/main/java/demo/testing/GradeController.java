ed442040d442169daf5e482e65745526cd1f2877
Somkiat <somkiat.p@gmail.com>
2013-11-15 12:57:23 +0700

    Update controller

--- src/main/java/demo/testing/GradeController.java
+++ src/main/java/demo/testing/GradeController.java
@@ -25,7 +25,7 @@ public class GradeController extends HttpServlet {
 
 	private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		ServletOutputStream out = resp.getOutputStream();
-		out.println("TODO next 2");
+		out.println("TODO For Spartan Course at Phuket");
 		out.flush();
 		out.close();
 	}
