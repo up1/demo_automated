dd0596b51eb168d928e5a332698f455731165948
Somkiat <somkiat.p@gmail.com>
2013-12-20 08:50:34 +0700

    Start to demo at KBank

--- src/main/java/demo/testing/GradeCoverter.java
+++ src/main/java/demo/testing/GradeCoverter.java
@@ -16,7 +16,7 @@ public class GradeCoverter {
 			else if(score >= 70) return "B";
 			else if(score >= 60) return "C";
 			else if(score >= 50) return "D";
-			return "C";
+			return "F";
 		} else {
 			return "Unknown";
 		}
