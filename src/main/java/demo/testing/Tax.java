1f59245b949690db9df0db350c68ebc4459b6726
Somkiat <somkiat.p@gmail.com>
2014-04-23 23:58:39 +0700

    Refactoring  code to remove duplicate return

--- src/main/java/demo/testing/Tax.java
+++ src/main/java/demo/testing/Tax.java
@@ -2,20 +2,23 @@ package demo.testing;
 
 public class Tax {
 
+	private static final int MIN_SALARY = 150000;
+
 	double[] salaryStep = { 150000, 200000, 250000, 250000, 1000000, 2000000 };
 
-	double[] taxStep = { 0.05, 0.10, 0.15, 0.20, 0.25, 0.30 };
+	double[] taxStep = { 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35 };
 
 	public double compute(int income) {
-		if (income > 150000) {
-			return calculateTax(income - 150000, 0);
+		double summaryTax = 0;
+		if (income > MIN_SALARY) {
+			summaryTax = calculateTax(income - MIN_SALARY, 0);
 		}
-		return 0;
+		return summaryTax;
 	}
 
 	private double calculateTax(double remainIncome, int row) {
 		if (row == 6) {
-			return remainIncome * 0.35;
+			return remainIncome * taxStep[row];
 		} else {
 			if (remainIncome - salaryStep[row] < 0)
 				return remainIncome * taxStep[row];
