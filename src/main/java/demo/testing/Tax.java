5d5008b75a6755948edc5e504c62b5ce0a4f6953
Somkiat <somkiat.p@gmail.com>
2014-04-23 22:53:58 +0700

    Resolve case 4

--- src/main/java/demo/testing/Tax.java
+++ src/main/java/demo/testing/Tax.java
@@ -3,6 +3,14 @@ package demo.testing;
 public class Tax {
 
 	public double compute(int income) {
+		if(income > 300000) {
+			income = income - 150000;
+			if(income > 150000) {
+				return  7500 +  (income - 150000) * (0.10);
+			} else {
+				return (income - 150000) * (0.05);
+			}
+		}
 		if(income > 150000) {
 			return (income - 150000) * (0.05);
 		}
