3e1d970bf8f94a3d3922808809b539d054bd7886
Somkiat <somkiat.p@gmail.com>
2014-04-23 22:44:24 +0700

    Add case 2

--- src/test/java/demo/testing/MyTaxTest.java
+++ src/test/java/demo/testing/MyTaxTest.java
@@ -7,11 +7,19 @@ import org.junit.Test;
 public class MyTaxTest {
 
 	@Test
-	public void salaryEqual150kBAHTShouldNotHaveTax() {
+	public void salaryEqual150kBahtShouldNotHaveTax() {
 		double expectedResult = 0;
 		Tax tax = new Tax();
 		double actualTax = tax.compute(150000);
-		assertEquals(expectedResult, actualTax);
+		assertEquals(expectedResult, actualTax, 2);
+	}
+	
+	@Test
+	public void salaryEqual200kBahtShouldHaveTax5000Baht() {
+		double expectedResult = 5000;
+		Tax tax = new Tax();
+		double actualTax = tax.compute(200000);
+		assertEquals(expectedResult, actualTax, 2);
 	}
 
 }
